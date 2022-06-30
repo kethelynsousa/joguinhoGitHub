19f2d5d95699558a3d0d940070fa3b2ec3e7172c
Aluno <Kethelyn@aluno.senai.br>
2022-06-30 08:49:46 -0300

    encerrei até aqui..necessito contratar um programador fullstack c#

--- js/escrevendo-aula1.js
+++ js/escrevendo-aula1.js
@@ -1,15 +1,16 @@
 //o que é uma variável?
 /*
-a variável é um espaço reservado na memória do computador.
-vamos declarar variáveis no JavaScript?
-
+  A variável é um espaço reservado na memória
+  do computador.
+  Vamos declarar variáveis no JavaScript?
 */
 
-var button = document.querySelector("buttor");
+var button = document.querySelector("button");
 
 button.onclick = function() {
     var nome = prompt("Qual é o comando para iniciar o git?");
     if ((nome = "git init")) {
-        alert("O" + nome + ", serve para iniciar o git.Parabéns você acertou!")
+        alert("O" + nome + ", serve para iniciar o git.Parabéns você acertou!");
     }
-}
+    window.location = "aula2.html";
+};
